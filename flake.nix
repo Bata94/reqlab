351f0eced29e1f8f2cf3af163851913fc108a96c
Bastian Sievers <b.sievers@sits.ruhr>
2024-12-27 23:50:32 +0100

    added flake.nix & .gitignore

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "ReqLab Dev Shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            air
+            sqlc
+            goose
+          ];
+          shellHook = ''
+            go mod tidy
+          '';
+        };
+      });
+}
+
